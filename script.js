3e14f4ccd01750f6d8cfede502ddfd05cb3edef6
Kai Xiang <anythinnk@live.com>
2020-08-12 18:33:25 +0800

    Added functions to toggle read status and remove book from library.

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
 let myLibrary = [];
 
-function Book(title, author, pages, read = false) {
+function Book(title, author, pages, read = false, inLibrary = false) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.inLibrary = inLibrary;
 }
 
 Book.prototype.info = function() {
     return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? 'read!' : 'not read yet.'}`
 }
 
+Book.prototype.remove = function() {
+    if (this.inLibrary) {
+        myLibrary.splice(myLibrary.indexOf(this), 1);
+        this.inLibrary = false;
+    }
+}
+
+Book.prototype.toggleRead = function() {
+    this.read = !this.read;
+}
+
 function addBooksToLibrary() {
     for (let i = 0; i < arguments.length; i++) {
         let elem = arguments[i];
-        myLibrary.push(elem);
+        if (!elem.inLibrary) {
+            myLibrary.push(elem);
+            elem.inLibrary = true;
+        }
     }
 }
